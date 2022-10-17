d86b17218579254c8691414d476be64d390f6138
kamaeff <kamaeff2@gmail.com>
2022-10-17 14:32:06 +0300

    first commit

---
+++ script/app.js
@@ -0,0 +1,28 @@
+const langCurrent = document.querySelector('.list__item--active');
+const languages = document.querySelectorAll('.list__item');
+const take = document.querySelector('.list__take');
+
+// Попробуйте её самостоятельно разобрать
+const siblings = el => [].slice.call(el.parentNode.children).filter(child => (child !== el)); // объявляем функцию, которая будет возвращать соседние элементы
+
+languages.forEach(lang => {
+    lang.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        if (lang.classList.contains('list__item--active')) {
+            siblings(lang).forEach(el => { // для каждого соседнего элемента
+                el.classList.add('list__item--show') // добавляем класс для показа
+            })
+        } else {
+            lang.classList.add('list__item--active'); // добавляем класс активности
+            lang.classList.remove('list__item--show'); // удаляем класс для показа
+            
+
+            siblings(lang).forEach(el => {
+                el.classList.remove('list__item--active');
+                el.classList.remove('list__item--show');
+            })
+        }
+
+    });
+
+});
